1cd86c8ad02eb23665f3a0e68701eeb2c0436f22
Stratton Sloane <stratton.s@outlook.com>
2020-06-25 16:28:36 +1000

    Display buildings and resources

--- src/Node.cs
+++ src/Node.cs
@@ -8,7 +8,7 @@ namespace Industropolis
         private List<Node> _children = new List<Node>();
 
         public Vector2 Position { get; set; }
-        public Vector2 GlobalPosition => Parent != null ? Position + Parent.Position : Position;
+        public Vector2 GlobalPosition => Parent != null ? Position + Parent.GlobalPosition : Position;
 
         public Node? Parent { get; set; }
         public IReadOnlyList<Node> Children => _children;
