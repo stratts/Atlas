4bcc5ace6c6e1118805889375ad984da9a70d71c
Stratton Sloane <stratton.s@outlook.com>
2020-06-27 12:47:44 +1000

    Move HSV color from extension to dedicated struct

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -16,75 +16,26 @@ namespace Industropolis
         }
     }
 
-    public static class ColorExtensions
+    public struct ColorHsv
     {
-        public static float GetHue(this Color c)
+        public float Hue { get; set; }
+        public float Saturation { get; set; }
+        public float Value { get; set; }
+
+        public ColorHsv(float hue, float saturation, float value)
         {
-            var (h, _, _) = c.GetHsv();
-            return h;
+            Hue = hue;
+            Saturation = saturation;
+            Value = value;
         }
 
-        public static void SetHue(this ref Color c, float hue)
+        public Color ToRgb()
         {
-            var (_, s, v) = c.GetHsv();
-            c.SetHsv(hue, s, v);
+            var (r, g, b) = HsvToRgb(Hue, Saturation, Value);
+            return new Color((byte)Math.Round(r), (byte)Math.Round(g), (byte)Math.Round(b));
         }
 
-        public static float GetSaturation(this Color c)
-        {
-            var (_, s, _) = c.GetHsv();
-            return s;
-        }
-
-        public static void SetSaturation(this ref Color c, float saturation)
-        {
-            var (h, _, v) = c.GetHsv();
-            c.SetHsv(h, saturation, v);
-        }
-
-        public static float GetValue(this Color c)
-        {
-            var (_, _, v) = c.GetHsv();
-            return v;
-        }
-
-        public static void SetValue(this ref Color c, float value)
-        {
-            var (h, s, _) = c.GetHsv();
-            c.SetHsv(h, s, value);
-        }
-
-        private static (float h, float s, float v) GetHsv(this Color c) => RgbToHsv(c.R, c.G, c.B);
-
-        private static void SetHsv(this ref Color c, float h, float s, float v)
-        {
-            var (r, g, b) = HsvToRgb(h, s, v);
-            c.R = (byte)r;
-            c.G = (byte)g;
-            c.B = (byte)b;
-        }
-
-        public static (float h, float s, float v) RgbToHsv(float r, float g, float b)
-        {
-            var max = Math.Max(Math.Max(r, g), b);
-            var min = Math.Min(Math.Min(r, g), b);
-            var c = max - min;
-
-            float hueDeriv = 0;
-
-            if (c == 0) hueDeriv = 0;
-            else if (max == r) hueDeriv = ((g - b) / c) % 6;
-            else if (max == g) hueDeriv = (b - r) / c + 2;
-            else if (max == b) hueDeriv = (r - g) / c + 4;
-
-            float hue = hueDeriv * 60;
-            float value = max / 255f;
-            float sat = max == 0 ? 0 : (c / 255f) / value;
-
-            return (hue / 360f, sat, value);
-        }
-
-        public static (float r, float g, float b) HsvToRgb(float h, float s, float v)
+        private static (float r, float g, float b) HsvToRgb(float h, float s, float v)
         {
             float c = v * s;
             float hDeriv = (h * 360) / 60;
@@ -102,6 +53,35 @@ namespace Industropolis
             var m = v - c;
 
             return ((set1.r + m) * 255, (set1.g + m) * 255, (set1.b + m) * 255);
+        }
+    }
+
+    public static class ColorExtensions
+    {
+        public static ColorHsv ToHsv(this Color c)
+        {
+            var (h, s, v) = RgbToHsv(c.R, c.G, c.B);
+            return new ColorHsv(h, s, v);
+        }
+
+        private static (float h, float s, float v) RgbToHsv(float r, float g, float b)
+        {
+            var max = Math.Max(Math.Max(r, g), b);
+            var min = Math.Min(Math.Min(r, g), b);
+            var c = max - min;
+
+            float hueDeriv = 0;
+
+            if (c == 0) hueDeriv = 0;
+            else if (max == r) hueDeriv = ((g - b) / c) % 6;
+            else if (max == g) hueDeriv = (b - r) / c + 2;
+            else if (max == b) hueDeriv = (r - g) / c + 4;
+
+            float hue = hueDeriv * 60;
+            float value = max / 255f;
+            float sat = max == 0 ? 0 : (c / 255f) / value;
+
+            return (hue / 360f, sat, value);
         }
     }
 }
