52b348b0e4476949b1e2184b6717471bfccce4f6
Stratton Sloane <stratton.s@outlook.com>
2020-06-27 20:08:47 +1000

    MapDisplay inherits from Scene

--- src/Scene.cs
+++ src/Scene.cs
@@ -7,6 +7,7 @@ namespace Industropolis
     public class Scene
     {
         private List<IDrawable> _drawable = new List<IDrawable>();
+        protected Vector2 _camera;
 
         public void AddNode(Node node)
         {
@@ -15,11 +16,16 @@ namespace Industropolis
             foreach (var child in node.Children) AddNode(child);
         }
 
-        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        public virtual void Update(float elapsed)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
         {
             foreach (var d in _drawable)
             {
-                var pos = d.GlobalPosition.Floor() - position.Floor();
+                var pos = d.GlobalPosition.Floor() - _camera.Floor();
 
                 if (!(pos.Y + d.Size.Y < 0 || pos.X + d.Size.X < 0 || pos.X > 1280 || pos.Y > 720))
                     d.Draw(spriteBatch, pos);
