ca5e48916e4fce54e3700a9a4bdefbbdc5ee72c2
Stratton Sloane <stratton.s@outlook.com>
2020-06-27 15:09:30 +1000

    Add PathNode vis

--- src/CustomDrawing.cs
+++ src/CustomDrawing.cs
@@ -6,19 +6,32 @@ namespace Industropolis
 {
     public static class CustomDrawing
     {
+        private static Texture2D _circleTexture = null!;
         private static Texture2D _texture = null!;
         private static SpriteBatch _spriteBatch = null!;
 
         public static void Init(SpriteBatch spriteBatch)
         {
+            _spriteBatch = spriteBatch;
             _texture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
             _texture.SetData(new Color[] { Color.White });
-            _spriteBatch = spriteBatch;
+            _circleTexture = CreateCircleTexture(128);
         }
 
         public static void DrawRect(Vector2 position, Vector2 size, Color color)
         {
             _spriteBatch.Draw(_texture, new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), color);
+        }
+
+        public static void DrawCircle(Vector2 position, int radius, Color color)
+        {
+            _spriteBatch.Draw(_circleTexture,
+                new Rectangle(
+                    (int)position.X - radius,
+                    (int)position.Y - radius,
+                    radius * 2,
+                    radius * 2),
+                    color);
         }
 
         public static void DrawLine(Vector2 start, Vector2 end, int width, Color color)
@@ -38,6 +51,29 @@ namespace Industropolis
                 effects: SpriteEffects.None,
                 layerDepth: 0
             );
+        }
+
+        private static Texture2D CreateCircleTexture(int size)
+        {
+            var colors = new Color[size * size];
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    var idx = x + size * y;
+                    var r = size / 2;
+                    var xc = x - r;
+                    var yc = y - r;
+
+                    if (xc * xc + yc * yc <= r * r) colors[idx] = Color.White;
+                    else colors[idx] = Color.Transparent;
+                }
+            }
+
+            Texture2D texture = new Texture2D(_spriteBatch.GraphicsDevice, size, size);
+            texture.SetData(colors);
+            return texture;
         }
     }
 }
