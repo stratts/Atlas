c185493cbf0ce52c76e40f6fa425a3c07a10ab64
Stratton Sloane <stratton.s@outlook.com>
2020-06-29 20:52:34 +1000

    Draw equilateral triangles

--- src/CustomDrawing.cs
+++ src/CustomDrawing.cs
@@ -7,6 +7,7 @@ namespace Industropolis
     public static class CustomDrawing
     {
         private static Texture2D _circleTexture = null!;
+        private static Texture2D _triangleTexture = null!;
         private static Texture2D _texture = null!;
         private static SpriteBatch _spriteBatch = null!;
 
@@ -15,7 +16,8 @@ namespace Industropolis
             _spriteBatch = spriteBatch;
             _texture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
             _texture.SetData(new Color[] { Color.White });
-            _circleTexture = CreateCircleTexture(128);
+            _circleTexture = CreateCircleTexture(64);
+            _triangleTexture = CreateTriangleTexture(64);
         }
 
         public static void DrawRect(Vector2 position, Vector2 size, Color color)
@@ -48,6 +50,20 @@ namespace Industropolis
                     color);
         }
 
+        public static void DrawEqTriangle(Vector2 basePos, Vector2 size, Color color)
+        {
+            _spriteBatch.Draw(
+                _triangleTexture,
+                destinationRectangle: new Rectangle((int)basePos.X, (int)basePos.Y, (int)size.X, (int)size.Y),
+                sourceRectangle: null,
+                color: color,
+                rotation: 0,
+                origin: new Vector2(0.5f, 1f),
+                effects: SpriteEffects.None,
+                layerDepth: 0
+            );
+        }
+
         public static void DrawLine(Vector2 start, Vector2 end, int width, Color color)
         {
             var dist = end - start;
@@ -64,6 +80,25 @@ namespace Industropolis
                 effects: SpriteEffects.None,
                 layerDepth: 0
             );
+        }
+
+        private static Texture2D CreateTriangleTexture(int size)
+        {
+            var colors = new Color[size * size];
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    var idx = x + size * y;
+                    if (Math.Abs(x - size / 2) <= Math.Abs(y / 2)) colors[idx] = Color.White;
+                    else colors[idx] = Color.Transparent;
+                }
+            }
+
+            Texture2D texture = new Texture2D(_spriteBatch.GraphicsDevice, size, size);
+            texture.SetData(colors);
+            return texture;
         }
 
         private static Texture2D CreateCircleTexture(int size)
---
+++ src/EqTriangle.cs
@@ -0,0 +1,24 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Industropolis
+{
+    public class EqTriangle : Node, IDrawable
+    {
+        public Vector2 Size { get; set; }
+        public Color Color { get; set; }
+
+        public EqTriangle(Vector2 size, Color color)
+        {
+            Size = size;
+            Color = color;
+        }
+
+        public EqTriangle(Vector2 position, Vector2 size, Color color) : this(size, color)
+        {
+            Position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position) => CustomDrawing.DrawEqTriangle(position, Size, Color);
+    }
+}
