fc2b8c1f9b8c13f859961822ab0327bec5345d8e
Stratton Sloane <stratton.s@outlook.com>
2020-08-16 11:19:35 +1000

    Implement text input

---
+++ src/Controls/TextInput.cs
@@ -0,0 +1,121 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Industropolis.Engine
+{
+    public class TextInput : CustomDrawingNode
+    {
+        private Text _text;
+        private int _pointer = 0;
+        private KeyboardState prevState;
+        private (int, int)? _selected;
+
+        public TextInput()
+        {
+            _text = new Text();
+            AddChild(_text);
+
+            IndustropolisGame.CurrentWindow.TextInput += (obj, args) =>
+            {
+                HandleInput(args.Character, args.Key);
+            };
+
+            AddComponent(new Updateable() { UpdateMethod = Update });
+
+            var input = new MouseInput();
+            input.OnClick = pos =>
+            {
+                _selected = null;
+                _pointer = _text.IndexAt(pos.X);
+            };
+
+            input.OnMove = (pos, _) => HandleMove(input, pos);
+            AddComponent(input);
+        }
+
+        public override void Draw()
+        {
+            if (_selected.HasValue)
+            {
+                var start = GetPositionAt(_selected.Value.Item1);
+                var end = GetPositionAt(_selected.Value.Item2);
+                DrawRect(new Vector2(start, 0), new Vector2(end - start, _text.LineHeight), Color.OrangeRed * 0.8f);
+            }
+            else
+            {
+                var pointerPos = GetPositionAt(_pointer);
+                DrawLine(new Vector2(pointerPos, 0), new Vector2(pointerPos, _text.LineHeight), 2, Color.White);
+            }
+        }
+
+        private float GetPositionAt(int index)
+        {
+            var pointerText = _text.Content.AsSpan().Slice(0, index);
+            return _text.MeasureString(pointerText).X;
+        }
+
+        private void Update(float elapsed)
+        {
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Left) && !prevState.IsKeyDown(Keys.Left) && _pointer > 0)
+            {
+                _selected = null;
+                _pointer--;
+            }
+            if (state.IsKeyDown(Keys.Right) && !prevState.IsKeyDown(Keys.Right) && _pointer < _text.Content.Length)
+            {
+                _selected = null;
+                _pointer++;
+            }
+            prevState = state;
+        }
+
+        private void HandleMove(MouseInput input, Vector2 pos)
+        {
+            if (input.ButtonHeld)
+            {
+                input.CaptureGlobal = true;
+                var selectTo = _text.IndexAt(pos.X);
+                _selected = (Math.Min(_pointer, selectTo), Math.Max(_pointer, selectTo));
+                Console.WriteLine(_selected);
+            }
+            else
+            {
+                input.CaptureGlobal = false;
+            }
+        }
+
+        private void ClearSelected()
+        {
+            if (!_selected.HasValue) return;
+            var (start, end) = _selected.Value;
+            _text.Content = _text.Content.Remove(start, end - start);
+            _pointer = start;
+            _selected = null;
+        }
+
+        private void HandleInput(char character, Keys key)
+        {
+            if (Char.IsControl(character))
+            {
+                if (key == Keys.Back)
+                {
+                    if (_selected.HasValue) ClearSelected();
+                    else if (_pointer > 0)
+                    {
+                        _text.Content = _text.Content.Remove(_pointer - 1, 1);
+                        _pointer--;
+                    }
+                }
+            }
+            else
+            {
+                ClearSelected();
+                _text.Content = _text.Content.Insert(_pointer, character.ToString());
+                _pointer++;
+            }
+            Size = _text.Size;
+        }
+    }
+}
--- src/Drawing/Text.cs
+++ src/Drawing/Text.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,12 +10,53 @@ namespace Industropolis.Engine
         public Color Color { get; set; } = Color.White;
         public int FontSize { get; set; } = 16;
 
-        public override Vector2 Size => MeasureSize();
+        public override Vector2 Size => MeasureString(Content);
 
         public Text()
         {
             AddComponent(new Drawable() { Draw = Draw });
             AddComponent<Modulate>();
+        }
+
+        public int LineHeight
+        {
+            get
+            {
+                FontService.SetSize(FontSize);
+                return FontService.Height;
+            }
+        }
+
+        public int IndexAt(float x)
+        {
+            FontService.SetSize(FontSize);
+            Vector2 pos = new Vector2(0, FontService.Ascender);
+            int idx = 0;
+            foreach (var c in Content)
+            {
+                var glyph = FontService.GetGlyph(c);
+                pos.X += glyph.Advance + glyph.Kerning;
+                if (pos.X > x) break;
+                idx++;
+            }
+            return idx;
+        }
+
+        public Vector2 MeasureString(ReadOnlySpan<char> s)
+        {
+            FontService.SetSize(FontSize);
+            Vector2 pos = new Vector2(0, FontService.Ascender);
+            foreach (var c in s)
+            {
+                if (c == '\n')
+                {
+                    pos = new Vector2(0, pos.Y + FontService.Height);
+                    continue;
+                }
+                var glyph = FontService.GetGlyph(c);
+                pos.X += glyph.Advance + glyph.Kerning;
+            }
+            return new Vector2(pos.X, pos.Y + FontService.NominalHeight - FontService.Ascender);
         }
 
         private void Draw(SpriteBatch spriteBatch, Vector2 position)
@@ -35,23 +77,6 @@ namespace Industropolis.Engine
 
                 pos.X += glyph.Advance + glyph.Kerning;
             }
-        }
-
-        private Vector2 MeasureSize()
-        {
-            FontService.SetSize(FontSize);
-            Vector2 pos = new Vector2(0, FontService.Ascender);
-            foreach (var c in Content)
-            {
-                if (c == '\n')
-                {
-                    pos = new Vector2(0, pos.Y + FontService.Height);
-                    continue;
-                }
-                var glyph = FontService.GetGlyph(c);
-                pos.X += glyph.Advance + glyph.Kerning;
-            }
-            return new Vector2(pos.X, pos.Y + FontService.NominalHeight - FontService.Ascender);
         }
     }
 }
