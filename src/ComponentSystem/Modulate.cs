6d03315e1bc26c59d2ae3661c0273c78ed60cf18
Stratton Sloane <stratton.s@outlook.com>
2021-07-23 12:30:54 +1000

    git subrepo push Engine
    subrepo:
      subdir:   "Engine"
      merged:   "2604285"
    upstream:
      origin:   "ssh://git@miserver:222/stratts/Atlas.git"
      branch:   "master"
      commit:   "2604285"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- src/ComponentSystem/Modulate.cs
+++ src/ComponentSystem/Modulate.cs
@@ -16,7 +16,7 @@ namespace Atlas
         {
             if (!Enabled) return color;
             var opacity = InheritFrom != null ? Opacity * InheritFrom.Opacity : Opacity;
-            var tint = InheritFrom != null && InheritFrom.Tint != Color.White ? InheritFrom.Tint : Tint;
+            var tint = InheritFrom != null && InheritFrom.Tint != Colors.White ? InheritFrom.Tint : Tint;
             var brightness = InheritFrom != null ? InheritFrom.Brightness + Brightness : Brightness;
             var c = color;
             var cHsv = c.ToHsv();
